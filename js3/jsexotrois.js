7c20d0224a214d690247a54442e8e3f9dd1aa07a
nel <nawfal.elkhaznagi@gmail.com>
2024-09-05 14:07:03 +0200

    debut exercice 2

--- js3/jsexotrois.js
+++ js3/jsexotrois.js
@@ -3,20 +3,36 @@ let btnAdd = document.querySelector(".btn");
 let btnDel = document.querySelector(".btn-del");
 let inputField = document.querySelector("input");
 let inputFieldNumber = document.querySelector("#numberElement");
-// let inputField = document.createElement("input");
 
-btnAdd.addEventListener("click", () => {
+let elementGenerator = function () {
   let fieldNumber = +inputFieldNumber.value;
   for (let count = 0; count <= fieldNumber; count++) {
-    let childListE = document.createElement("li");
-    childListE.innerHTML = inputField.value;
-    myList.appendChild(childListE);
+    let myDate = new Date();
+    myDate.toLocaleString();
+    console.log(myDate.toLocaleDateString());
+    // setTimeout(() => {
+    //   let childListE = document.createElement("li");
+    //   childListE.innerHTML = `${inputField.value} ${
+    //     day + myDays
+    //   }/${month}/${myDate.getFullYear()}`;
+
+    //   myList.appendChild(childListE);
+    //   myDays++;
+    // }, count * 500);
   }
-});
+};
+
+// let inputField = document.createElement("input");
+
+btnAdd.addEventListener("click", elementGenerator);
 
 btnDel.addEventListener("click", () => {
   if (myList.children.length != 0) {
     console.log("no child to remove");
   }
-  myList.removeChild(myList.lastChild);
+
+  let fieldNumber = +inputFieldNumber.value;
+  for (let i = 0; i <= fieldNumber; i++) {
+    myList.removeChild(myList.lastChild);
+  }
 });
