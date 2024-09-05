bce193bfd6db933b2ed841b30da3e9876da1de28
nel <nawfal.elkhaznagi@gmail.com>
2024-09-05 14:25:13 +0200

    started working on exercice 3

---
+++ js4/index.js
@@ -0,0 +1,24 @@
+let addBtns = document.querySelectorAll(".addBtn");
+
+let total = 0;
+let product1 = {
+  name: "Chaise",
+  price: 25,
+  quantity: 0,
+};
+let product2 = {
+  name: "tabouret",
+  price: 15,
+  quantity: 0,
+};
+let product3 = {
+  name: "table",
+  price: 100,
+  quantity: 0,
+};
+
+addBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // fait ce que je veux
+  });
+});
